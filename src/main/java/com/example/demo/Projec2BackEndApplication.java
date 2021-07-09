829e050a913298dafdf7055f16f04cf932d6801f
Matt Anderson <manderson0182@gmail.com>
2021-07-09 08:32:21 -0600

    first commit, created spring starter project

---
+++ src/main/java/com/example/demo/Projec2BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projec2BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projec2BackEndApplication.class, args);
+	}
+
+}
