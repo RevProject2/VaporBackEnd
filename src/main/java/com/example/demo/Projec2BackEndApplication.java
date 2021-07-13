350b57ab0bb652db0d28f3127156c2e5443bfe27
Matt Anderson <manderson0182@gmail.com>
2021-07-13 13:56:01 -0600

    added to the pom.xml, and demo to enable Jenkins

--- src/main/java/com/example/demo/Projec2BackEndApplication.java
+++ src/main/java/com/example/demo/Projec2BackEndApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Projec2BackEndApplication {
+public class Projec2BackEndApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projec2BackEndApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Projec2BackEndApplication.class);
 	}
 
 }
