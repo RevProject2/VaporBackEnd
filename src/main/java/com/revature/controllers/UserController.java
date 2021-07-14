158dc681f32d323dd54b8145ee0640b3dad29712
Seth Knox <seth.r.knox@gmail.com>
2021-07-14 16:13:36 -0500

    adding crossorigin annotation to userclass

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.revature.services.UserService;
 
 @RestController
 @RequestMapping(path="/users")
+@CrossOrigin
 public class UserController {
 
 	private UserService us;
