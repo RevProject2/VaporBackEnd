e0bf12b3a19439d80c40dda9a55dcb08b06f2de5
Seth Knox <seth.r.knox@gmail.com>
2021-07-15 16:41:44 -0500

    changing back to JAR

--- src/main/java/com/example/demo/Projec2BackEndApplication.java
+++ src/main/java/com/example/demo/Projec2BackEndApplication.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,16 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.revature")
 @EnableJpaRepositories("com.revature.repos")
 @EntityScan("com.revature.beans")
-public class Projec2BackEndApplication extends SpringBootServletInitializer {
+public class Projec2BackEndApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projec2BackEndApplication.class, args);
 	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Projec2BackEndApplication.class);
-	}
+
 
 
 }
